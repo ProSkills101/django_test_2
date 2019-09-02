79b440366bb1a8b3ed74082caa2fb4570db082f5
Jim Wang <tytnoob000@gmail.com>
2019-09-02 17:10:01 +0800

    carousel-model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Carousel
+admin.site.register(Carousel)
 # Register your models here.
--- main/views.py
+++ main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Carousel
+
+
+    
+
 
 def index(request):
-    context = {'title': 'Hello First Page'}
+    carousels = Carousel.objects.order_by('-pub_date')[:5]
+    context = {'carousels': carousels}
+    print("XXXXXXXXXX",context)
     return render(request, 'main/index.html', context)
 # Create your views here.
